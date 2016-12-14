475cdf8f6bd8623968ab2b91609c5a0254ce0f12
tneedham <tneedham@TNEEDHAM.meridian.local>
2016-12-14 18:59:04 +0000

    Day 14 with sliding windows and parallelism.

--- AdventOfCode2016/Day14.fs
+++ AdventOfCode2016/Day14.fs
@@ -1,78 +1,65 @@
 ﻿module Day14
 
+open Array.Parallel;
 open System;
 open System.IO;
 open System.Security.Cryptography;
 open System.Text;
 
-type Key = { Index : int; Hash : string };;
+[<StructuredFormatDisplay("{AsString}")>]
+type Key = { Index : int; Hash : string }
+            override m.ToString() = String.Format("Index {0} Hash {1}", m.Index, m.Hash)
+            member m.AsString = m.ToString();;
 
-type Candidate = { Token : char; Value : Key };;
+[<StructuredFormatDisplay("{AsString}")>]
+type Candidate = { Token : char; Value : Key }
+                    override m.ToString() = String.Format("Token {0} {1}", m.Token, m.Value)
+                    member m.AsString = m.ToString();;
 
-let md5 (d : byte array) : byte array =
+let md5 (s : string) : string =
     use md5 = MD5.Create();
-    md5.ComputeHash(d);
-
-let bytesToHex (b : byte array) : string =
-    b
+    s
+    |> Encoding.ASCII.GetBytes
+    |> md5.ComputeHash
     |> Seq.map (fun x -> String.Format("{0:x2}", x))
     |> String.Concat;
 
-let rec token (n : int) (f : char) (t : int) (c : char list) : char option =
-    match c with
-        | [] -> None;
-        | x::xs ->  if x = f then
-                        if t + 1 = n then
-                            Some f;
-                        else
-                            token n f (t+1) xs;
-                    else
-                        token n x 1 xs;
+let nplet (s : string) (n : int) : char option = 
+    s
+    |> Seq.windowed n
+    |> Seq.tryFind (fun t -> [0..n-1] |> List.fold (fun a c -> a && t.[0] = t.[c]) true)
+    |> Option.map (fun u -> u.[0]);
 
 let rec hash (s : string) (n : int) : string =
     match n with
-        | 0 ->  s
-                |> Encoding.ASCII.GetBytes
-                |> md5
-                |> bytesToHex;
-        | _ -> hash s (n-1)
-                |> Encoding.ASCII.GetBytes
-                |> md5
-                |> bytesToHex;
+        | 0 -> md5 s;
+        | _ -> hash (md5 s) (n-1);
 
-let generate (s : string) (i : int) (n : int) : string =
-    hash (s + i.ToString()) n;
-
-let rec find (c : Candidate list) (o : Key list) (s : string) (i : int) (n : int) (z : int) : Key list =
-    match o.Length with
-        | y when y >= n ->  o
-                            |> Seq.sortBy (fun x -> x.Index)
-                            |> Seq.take n
+let rec find (cs : Candidate list) (ks : Key list) (s : string) (i : int) (k : int) (n : int) : Key list =
+    match List.length ks with
+        | l when l >= k ->  ks
+                            |> Seq.take k
                             |> Seq.toList
                             |> List.rev;
-        | _ ->  let cs = c
-                        |> Seq.filter (fun x -> i - x.Value.Index <= 1000)
-                        |> Seq.toList;
+        | _ ->  let ts = [|i..i+999|]
+                        |> Array.Parallel.map (fun x -> { Index = x; Hash = hash (s + x.ToString()) n })
+                        |> Array.Parallel.map (fun y -> (nplet y.Hash 3, y))
+                        |> Array.filter (fun (x, y) -> x <> None)
+                        |> Array.map (fun (Some x, y) -> { Token = x; Value = y})
+                        |> Array.toList;
 
-                let h = generate s i z;
-                let g = token 5 Char.MinValue 0 (h |> Seq.toList);
+                let fs = cs@ts
+                        |> List.map (fun x -> (nplet x.Value.Hash 5, x.Value.Index))
+                        |> List.filter (fun (x, y) -> x <> None)
+                        |> List.map (fun (Some x, y) -> (x, y))
+                        |> List.sort;
 
-                let p = o @ match g with
-                                | None -> [];
-                                | Some t -> cs 
-                                            |> Seq.filter (fun x -> x.Token = t) 
-                                            |> Seq.map (fun x -> x.Value) 
-                                            |> Seq.toList;
-                let d = match g with
-                            | None -> cs;
-                            | Some t -> cs
-                                        |> Seq.filter (fun x -> x.Token <> t)
-                                        |> Seq.toList;
+                let os = cs
+                        |> List.filter (fun x -> fs |> List.tryFindIndex (fun (y, z) -> y = x.Token && (z - x.Value.Index) > 0 && (z - x.Value.Index) <= 1000) <> None)
+                        |> List.map (fun x -> x.Value);
 
-                match token 3 Char.MinValue 0 (h |> Seq.toList) with
-                    | Some t -> let e = { Token = t; Value = { Index = i; Hash = h} };
-                                find (e::d) p s (i+1) n z;
-                    | None -> find d p s (i+1) n z;
+                find ts (ks@os) s (i+1000) k n;
+   
 
 let run (file : string) =
     let salt = (Seq.toList (File.ReadLines(file))).[0];
