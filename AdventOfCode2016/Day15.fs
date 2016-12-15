eca104d66cf03d4720fd246fc460d9917b13acc5
tneedham <tneedham@TNEEDHAM.meridian.local>
2016-12-15 09:13:52 +0000

    Day 15 brute force

---
+++ AdventOfCode2016/Day15.fs
@@ -0,0 +1,42 @@
+﻿module Day15
+
+open System;
+open System.IO;
+
+let parse (s : string) : int * int =
+    match s.Split([| ' '; '.' |], StringSplitOptions.RemoveEmptyEntries) with
+        | [|_; _; _; x; _; _; _; _; _; _; _; y |] -> (Int32.Parse y, Int32.Parse x);
+
+let pass (t : int) (s : int * int) : bool =
+    let (i, p) = s;
+    (i + t + 1) % p = 0;
+
+
+let rec attempt (t: int) (s : (int * int) list) : bool =
+    match s with
+        | [] -> true;
+        | x::xs ->  if pass t x then
+                        attempt (t+1) xs;
+                    else
+                        false;
+
+let rec drop (t : int) (s : (int * int) list) : int =
+    match attempt t s with
+        | true -> t;
+        | false -> drop (t+1) s;
+
+let run (file : string) =
+    let input = Seq.toList (File.ReadLines(file));
+    
+    let discs = input
+                |> List.map (fun x -> parse x);
+
+    drop 0 discs
+    |> printfn "Day 15, part 1: %d";
+
+    let part2 = "Disc #7 has 11 positions; at time=0, it is at position 0."
+
+    let discs2 = discs @ [parse part2];
+    
+    drop 0 discs2
+    |> printfn "Day 15, part 2: %d";
--- AdventOfCode2016/Program.fs
+++ AdventOfCode2016/Program.fs
@@ -10,7 +10,8 @@ let main argv =
     let problems = 
         [| 
             Day1.run; Day2.run; Day3.run; Day4.run; Day5.run; Day6.run; Day7.run;
-            Day8.run; Day9.run; Day10.run; Day11.run; Day12.run; Day13.run; Day14.run
+            Day8.run; Day9.run; Day10.run; Day11.run; Day12.run; Day13.run; Day14.run;
+            Day15.run
         |];
 
     while cont do
