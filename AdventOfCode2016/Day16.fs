9fcc242dadc27cc8c4e467818d44ef6ee1d964ce
tneedham <tneedham@TNEEDHAM.meridian.local>
2016-12-16 15:05:47 +0000

    Day 16 even faster

--- AdventOfCode2016/Day16.fs
+++ AdventOfCode2016/Day16.fs
@@ -1,42 +1,37 @@
 ﻿module Day16
 
+open System;
 open System.IO;
 
-let dragon (bs : bool list) : bool list =
-    bs @ (false :: (bs |> List.rev |> List.map (fun x -> not x)));
+let dragon (i : string) : string =
+    i + ("0"  + (i |> Seq.toList |> List.rev |> List.map (fun x -> if x = '1' then '0' else '1') |> String.Concat));
 
-let rec pad (n : int) (bs : bool list) : bool list =
-    if bs |> List.length >= n then
-        bs |> Seq.take n |> Seq.toList;
+let rec pad (n : int) (i : string) : string =
+    if i.Length >= n then
+        i.Substring(0, n);
     else
-        pad n (dragon bs);
+        pad n (dragon i);
 
-let rec check (bs : bool list) : bool list =
-    bs
+let rec check (i : string) : string =
+    i.ToCharArray()
     |> Seq.pairwise
     |> Seq.mapi (fun i x -> (i, x))
     |> Seq.filter (fun (i, _) -> i % 2 = 0)
-    |> Seq.map (fun (_, (x, y)) -> x = y)
-    |> Seq.toList;
+    |> Seq.map (fun (_, (x, y)) -> if x = y then '1' else '0')
+    |> String.Concat;
 
-let rec checkSum (bs : bool list) : bool list =
-    match bs |> List.length with
-        | n when n % 2 = 0 -> checkSum (check bs);
-        | _  -> bs;
+let rec checkSum (i : string) : string =
+    match i.Length with
+        | n when n % 2 = 0 -> checkSum (check i);
+        | _  -> i;
 
 let run (file : string) =
-    let input = (Seq.toList (File.ReadLines(file))).[0]
-                |> Seq.toList
-                |> List.map (fun x -> x = '1');
+    let input = (Seq.toList (File.ReadLines(file))).[0];
     
     pad 272 input 
     |> checkSum
-    |> List.map (fun x -> if x then '1' else '0')
-    |> Seq.fold (fun a c -> a + string c) ""
     |> printfn "Day 16, part 1: %s";
 
     pad 35651584 input 
     |> checkSum
-    |> List.map (fun x -> if x then '1' else '0')
-    |> Seq.fold (fun a c -> a + string c) ""
     |> printfn "Day 16, part 2: %s";
