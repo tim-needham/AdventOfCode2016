6c589707ad277a55551ad61aba6684e61b4b1156
tneedham <tneedham@TNEEDHAM.meridian.local>
2016-12-16 14:56:42 +0000

    Day 16

---
+++ AdventOfCode2016/Day16.fs
@@ -0,0 +1,42 @@
+﻿module Day16
+
+open System.IO;
+
+let dragon (bs : bool list) : bool list =
+    bs @ (false :: (bs |> List.rev |> List.map (fun x -> not x)));
+
+let rec pad (n : int) (bs : bool list) : bool list =
+    if bs |> List.length >= n then
+        bs |> Seq.take n |> Seq.toList;
+    else
+        pad n (dragon bs);
+
+let rec check (bs : bool list) : bool list =
+    bs
+    |> Seq.pairwise
+    |> Seq.mapi (fun i x -> (i, x))
+    |> Seq.filter (fun (i, _) -> i % 2 = 0)
+    |> Seq.map (fun (_, (x, y)) -> x = y)
+    |> Seq.toList;
+
+let rec checkSum (bs : bool list) : bool list =
+    match bs |> List.length with
+        | n when n % 2 = 0 -> checkSum (check bs);
+        | _  -> bs;
+
+let run (file : string) =
+    let input = (Seq.toList (File.ReadLines(file))).[0]
+                |> Seq.toList
+                |> List.map (fun x -> x = '1');
+    
+    pad 272 input 
+    |> checkSum
+    |> List.map (fun x -> if x then '1' else '0')
+    |> Seq.fold (fun a c -> a + string c) ""
+    |> printfn "Day 16, part 1: %s";
+
+    pad 35651584 input 
+    |> checkSum
+    |> List.map (fun x -> if x then '1' else '0')
+    |> Seq.fold (fun a c -> a + string c) ""
+    |> printfn "Day 16, part 2: %s";
--- AdventOfCode2016/Program.fs
+++ AdventOfCode2016/Program.fs
@@ -11,7 +11,7 @@ let main argv =
         [| 
             Day1.run; Day2.run; Day3.run; Day4.run; Day5.run; Day6.run; Day7.run;
             Day8.run; Day9.run; Day10.run; Day11.run; Day12.run; Day13.run; Day14.run;
-            Day15.run
+            Day15.run; Day16.run
         |];
 
     while cont do
