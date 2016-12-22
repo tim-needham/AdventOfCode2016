f5f40cc42de2788a6bbaa08b974559c7db91b25c
tim-needham <timothy@Tims-Mac.home>
2016-12-22 09:29:11 +0000

    Day 22 - part 1

--- AdventOfCode2016/Day22.fs
+++ AdventOfCode2016/Day22.fs
@@ -1,11 +1,51 @@
 ﻿module Day22
 
+open System;
 open System.IO;
 
-let run (file : string) =
-    let input = Seq.toList (File.ReadLines(file));
+type Node = {
+    Name : string;
+    X : int;
+    Y : int;
+    Size : int;
+    Used : int;
+    Avail : int;
+    Perc : int
+};;
 
-    0
+let parse (s : string) : Node = 
+    match s.Split([|' '|], StringSplitOptions.RemoveEmptyEntries) |> Array.toList with
+        | a::b::c::d::e::_ ->   let p = Int32.Parse (b.Substring(0, b.Length - 1));
+                                let q = Int32.Parse (c.Substring(0, c.Length - 1));
+                                let r = Int32.Parse (d.Substring(0, d.Length - 1));
+                                let s = Int32.Parse (e.Substring(0, e.Length - 1));
+                                let l = a.Split('-');
+                                let x = Int32.Parse (l.[1].Substring(1));
+                                let y = Int32.Parse (l.[2].Substring(1));
+                                {
+                                    Name = a;
+                                    X = x;
+                                    Y = y;
+                                    Size = p;
+                                    Used = q;
+                                    Avail = r;
+                                    Perc = s;
+                                }
+
+let validPairs (ns : Node list) : int =
+    [for i in ns -> [for j in ns -> (i, j)]]
+    |> List.collect id
+    |> List.filter (fun (x, _) -> x.Used > 0)
+    |> List.filter (fun (x, y) -> x.Name <> y.Name)
+    |> List.filter (fun (x, y) -> x.Used <= y.Avail)
+    |> List.length;
+
+let run (file : string) =
+    let input = Seq.toList (File.ReadLines(file))
+                |> List.skip 2
+                |> List.map (parse);
+
+    validPairs input
     |> printfn "Day 22, part 1: %d";
 
     0
